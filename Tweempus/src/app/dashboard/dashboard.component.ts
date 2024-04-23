9f190cd6e5886cc5d656edf4500d085e0b9bdb02
DC10134 <jaime.gonzalez.casero@realeites.com>
2024-04-23 16:46:46 +0200

    Add first call to service to Tweempus

--- Tweempus/src/app/dashboard/dashboard.component.ts
+++ Tweempus/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthorService } from '../shared/author/author.service';
 
 @Component({
   selector: 'tweempus-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  constructor(private authorService: AuthorService) { }
+
+  ngOnInit(): void {
+    /*Observable needs subscribe, creating a channel between the Observable and
+    whoever is consuming the Observable. Unsuscribe is recommended if we open
+    several channels
+    */
+   
+    this.authorService.getAuthor('1').subscribe(author => console.log(author));
+    //this.authorService.getAuthor('h').subscribe(author => console.log(author));
+  }
 }
--- Tweempus/src/app/shared/author/author.service.ts
+++ Tweempus/src/app/shared/author/author.service.ts
@@ -2,12 +2,41 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, catchError, map } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
+
+import { Author } from './author.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
 
-  constructor() { }
+  private url: string = 'http://localhost:3000/authors';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAuthor(id: string): Observable<Author> {
+    //we return an Observable because we're gonna concatenate several calls
+    let author: Author;
+
+    //get petition
+    return this.httpClient.get<Author>(this.url + '/' + id).pipe(
+      map(dbAuthor => {
+        author = new Author(dbAuthor.id, dbAuthor.fullName);
+        author.image = dbAuthor.image;
+        author.url = 'http://localhost:4200/author/'+dbAuthor.id;
+        return author;
+      }),
+      catchError(this.handleError)
+    );
+
+  }
+
+  handleError(error: any) {
+    let errMsg = (error.message) ? error.message : 
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    //errMsg = 'This is an error';
+    console.error(errMsg);
+    return throwError(() => errMsg);
+  }
 }
